122acc2f330896ed6ea589660b5b528aa739a41d
Akashkumar888 <akashgkr12@gmail.com>
2025-02-15 13:07:22 +0530

    update

--- DSA_2025/lowest_common_ancestors_BST.cpp
+++ DSA_2025/lowest_common_ancestors_BST.cpp
@@ -15,3 +15,17 @@ class Solution {
       }
   };
   
+// iterative ok
+  Node* LCA(Node* root, Node* n1, Node* n2) {
+        // code here
+        while(root){
+        if(root->data > n1->data && root->data > n2->data){
+            root=root->left;
+        }
+        else if(root->data < n1->data && root->data < n2->data){
+             root=root->right;
+        }
+        else break;
+        }
+        return root;
+    }
