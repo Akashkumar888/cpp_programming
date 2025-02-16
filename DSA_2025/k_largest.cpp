4d3e19dbd8bf97dc36eb75732a16497d96752c59
Akashkumar888 <akashgkr12@gmail.com>
2025-02-17 04:30:07 +0530

    update

--- DSA_2025/k_largest.cpp
+++ DSA_2025/k_largest.cpp
@@ -1,3 +1,22 @@
+
+lass Solution {
+    public:
+      vector<int> kLargest(vector<int>& arr, int k) {
+          // Your code here
+          vector<int>ans;
+          sort(arr.begin(),arr.end(),greater<>());
+          int i=0;
+          while(i<k){
+              ans.push_back(arr[i]);
+              i++;
+          }
+          return ans;
+      }
+  };
+
+
+
+
 class{
   public:
       vector<int> kLargest(vector<int>& arr, int k) {
@@ -13,3 +32,24 @@ class{
           return ans;
       }
 };
+
+
+
+
+
+vector<int> kLargest(vector<int>& arr, int k) {
+    // Your code here
+    vector<int>ans(k);
+    priority_queue<int,vector<int>,greater<int>>pq;
+    for(int i=0;i<arr.size();i++){
+        pq.push(arr[i]);
+        if(pq.size()>k)pq.pop();
+    }
+    int i=k-1;
+    while(!pq.empty()){
+        ans[i]=pq.top();
+        pq.pop();
+        i--;
+    }
+    return ans;
+} 
