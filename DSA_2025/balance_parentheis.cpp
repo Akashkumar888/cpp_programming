3907a3cd6459a70766ed93c27a55d05bcc5fc436
Akashkumar888 <akashgkr12@gmail.com>
2025-02-21 00:21:57 +0530

    update

---
+++ DSA_2025/balance_parentheis.cpp
@@ -0,0 +1,11 @@
+
+bool isBalanced(string& s) {
+  stack<char> st;
+  for (char &ch : s) {
+      if (!st.empty() && st.top() == '(' && ch == ')') st.pop();
+      else if (!st.empty() && st.top() == '{' && ch == '}') st.pop();
+      else if (!st.empty() && st.top() == '[' && ch == ']') st.pop();
+      else st.push(ch);
+  }
+  return st.empty();
+}
