141473c358ec6f00f330f3c0ab3bacaeaa205416
Akashkumar888 <akashgkr12@gmail.com>
2025-05-25 19:15:05 +0530

    second

--- DSA_2025/Pythagorean Triplet.cpp
+++ DSA_2025/Pythagorean Triplet.cpp
@@ -15,3 +15,23 @@ class Solution {
     }
 };
 
+
+
+class Solution {
+  public:
+    bool pythagoreanTriplet(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        unordered_map<int,int>mp;
+        for(auto &num:arr)mp[num*num]++;
+        for(int i=0;i<n-1;i++){
+            int a=arr[i];
+            for(int j=i+1;j<n;j++){
+                int b=arr[j];
+            if(mp.find(a*a+b*b)!=mp.end())return true;
+            }
+        }
+        return false;
+    }
+};
+
