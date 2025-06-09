d761aaa62fb8a5bff83f140337a2ec6338e7ee9d
Akashkumar888 <akashgkr12@gmail.com>
2025-06-09 11:19:45 +0530

    update

---
+++ DSA_2025/BST with Dead End.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+  public:
+    bool dfs(Node* root,int low,int high) {
+        if (root==NULL)return false;
+
+        // Dead end condition
+        if (low==high)return true;
+
+        // Recurse for left and right subtree
+        return dfs(root->left,low,root->data-1) || 
+               dfs(root->right,root->data+1,high);
+    }
+
+    bool isDeadEnd(Node* root) {
+        return dfs(root,1,INT_MAX);  // BST values ≥ 1
+    }
+};
