d458589607a301e2c7be1ddd63076f98290c369a
Akashkumar888 <akashgkr12@gmail.com>
2025-06-10 12:39:23 +0530

    update

--- DSA_2025/Exactly one swap.cpp
+++ DSA_2025/Exactly one swap.cpp
@@ -1,23 +1,20 @@
-
 class Solution {
   public:
     int countStrings(string &s) {
         // code here
         int n=s.size();
-        int total=n*(n-1)/2;
+        int totalSwap=n*(n-1)/2;
         vector<int>mp(26,0);
-        int count=0;
+        int duplicate=0;
         for(char &ch:s)mp[ch-'a']++;
         for(int i=0;i<26;i++){
             if(mp[i]>1){
-            count+=mp[i]*(mp[i]-1)/2;
+            duplicate+=mp[i]*(mp[i]-1)/2;
             }
         }
 // ✅ If no duplicates, return total (i.e., all swaps produce unique results)
-        if(count==0)return total;
+        if(duplicate==0)return total;
 // ✅ If some duplicates, then subtract invalid swaps, and add 1 for the original string itself (why?)
-        return total-count+1;
+        return totalSwap-duplicate+1;
     }
 };
-
-
