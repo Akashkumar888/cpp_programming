52c03629d28eb708b8a6d320b807240622b0f723
Akashkumar888 <akashgkr12@gmail.com>
2025-06-20 11:48:10 +0530

    update

--- DSA_2025/Group Balls by Sequence.cpp
+++ DSA_2025/Group Balls by Sequence.cpp
@@ -9,9 +9,9 @@ class Solution {
         if(n%k)return false;
         map<int,int>mp;
         for(int &num:arr)mp[num]++;
-        for (auto it=mp.begin();it!=mp.end();it++){
-            int start=it->first;
-            int count=it->second;
+        for (auto &it:mp){
+            int start=it.first;
+            int count=it.second;
             if (count>0){
                 // Try to build k consecutive numbers starting from 'start'
                 for(int i=0;i<k;i++){
@@ -25,4 +25,29 @@ class Solution {
     }
 };
 
+
+
+class Solution {
+  public:
+    bool validgroup(vector<int> &arr, int k) {
+        // code here
+        int n=arr.size();
+        if(n%k)return false;
+        multiset<int>ms;
+        for(int &num:arr)ms.insert(num);
+        while(!ms.empty()){
+            int start = *ms.begin();
+            int cnt = 0;
+            while(cnt < k){
+                if(ms.find(start)==ms.end()) return false;
+                else{
+                    ms.erase(ms.find(start));
+                    start++,cnt++;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
 
