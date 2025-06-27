076484ffd187f8a3d33295d74b5816ed253b96ee
Akashkumar888 <akashgkr12@gmail.com>
2025-06-27 11:57:55 +0530

    update

--- DSA_2025/Mobile numeric keypad.cpp
+++ DSA_2025/Mobile numeric keypad.cpp
@@ -46,3 +46,44 @@ class Solution {
 
 ✅ Final Space Complexity:O(n)  // both memo and call stack
 
+class Solution {
+  public:
+  typedef long long ll;
+//   Add "no move" (stay on same key)
+  vector<vector<int>> directions = {
+    {0,-1},{-1,0},{1,0},{0,1},{0,0}  // 👈 Added (0,0)
+};
+
+  ll solve(int i,int j,int len,int n,vector<vector<int>>&keypad,vector<vector<vector<ll>>>&dp){
+      if(i<0 || j<0 || i>=4 || j>=3 || keypad[i][j]==0)return 0;
+      if(len==n)return 1;
+      if(dp[i][j][len]!=-1)return dp[i][j][len];
+      ll count=0;
+      for(auto &dir:directions){
+          int new_i=i+dir[0];
+          int new_j=j+dir[1];
+          // increase len+1 each time 
+          count=count+solve(new_i,new_j,len+1,n,keypad,dp);
+      }
+       dp[i][j][len]=count;  // ✅ correct
+       return dp[i][j][len];   // ✅ returns correct value
+  }
+    int getCount(int n) {
+        // code here
+        vector<vector<int>>keypad ={
+            {1, 1, 1},
+            {1, 1, 1},
+            {1, 1, 1},
+            {0, 1, 0}};
+            ll total=0;
+        vector<vector<vector<ll>>>dp(4,vector<vector<ll>>(3,vector<ll>(n+1,-1)));
+        for(int i=0;i<4;i++){
+            for(int j=0;j<3;j++){
+                if(keypad[i][j]!=' '){
+                    total=total+solve(i,j,1,n,keypad,dp);
+                }
+            }
+        }
+        return (int)total;
+    }
+};
