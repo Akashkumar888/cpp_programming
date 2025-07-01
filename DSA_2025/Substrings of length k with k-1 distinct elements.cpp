29fec6e86e6c010e56508c0b7e3c0c99edd7fd0a
Akashkumar888 <akashgkr12@gmail.com>
2025-07-01 15:52:26 +0530

    update

--- DSA_2025/Substrings of length k with k-1 distinct elements.cpp
+++ DSA_2025/Substrings of length k with k-1 distinct elements.cpp
@@ -21,6 +21,29 @@ class Solution {
     }
 };
 
+
+class Solution {
+  public:
+    int substrCount(string &s, int k) {
+        // code here
+        int n=s.size();
+        unordered_map<char,int>mp;
+        int count=0;
+        int i=0,j=0;
+        while(j<n){
+            mp[s[j]]++;
+            if(j-i+1>k){
+                mp[s[i]]--;
+                if(mp[s[i]]==0)mp.erase(s[i]);
+                i++;
+            }
+            if(j-i+1==k && mp.size()==k-1)count++;
+            j++;
+        }
+        return count;
+    }
+};
+
 // 🔍 Time Complexity:
 // Each character enters and exits the window once → O(n)
 // mp.size() and mp.erase() are O(1) average-case (unordered_map)
@@ -61,6 +84,33 @@ class Solution {
     }
 };
 
+class Solution {
+  public:
+  int frequency(vector<int>&mp){
+      int freq=0;
+      for(int i=0;i<26;i++){
+          if(mp[i]!=0)freq++;
+      }
+      return freq;
+  }
+    int substrCount(string &s, int k) {
+        // code here
+        int n=s.size();
+        vector<int>mp(26,0);
+        int count=0;
+        int i=0,j=0;
+        while(j<n){
+            mp[s[j]-'a']++;
+            if(j-i+1>k){
+                mp[s[i]-'a']--;
+                i++;
+            }
+            if(j-i+1==k && frequency(mp)==k-1)count++;
+            j++;
+        }
+        return count;
+    }
+};
 
 
 // 🔍 Time Complexity:
