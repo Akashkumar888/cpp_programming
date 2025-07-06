fe39d2b7f2ab71401e06de66fbfafdf12f6ed626
Akashkumar888 <akashgkr12@gmail.com>
2025-07-06 16:58:04 +0530

    update

--- DSA_2025/Maximum Sum Combination.cpp
+++ DSA_2025/Maximum Sum Combination.cpp
@@ -88,3 +88,40 @@ class Solution {
 // 🔹 Total space = O(k)
 
 
+
+
+class Solution {
+  public:
+    vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+        int n = a.size();
+        sort(a.begin(), a.end(), greater<int>());
+        sort(b.begin(), b.end(), greater<int>());
+        typedef pair<int, pair<int, int>> P;
+        priority_queue<P> pq;
+        set<pair<int, int>> visited;
+        pq.push({a[0] + b[0], {0, 0}});
+        visited.insert({0, 0});
+        vector<int> ans;
+        while (ans.size() < k && !pq.empty()) {
+            auto top = pq.top(); pq.pop();
+            int sum = top.first;
+            int i = top.second.first;
+            int j = top.second.second;
+            ans.push_back(sum);
+            if (i + 1 < n && visited.count({i + 1, j}) == 0) {
+                pq.push({a[i + 1] + b[j], {i + 1, j}});
+                visited.insert({i + 1, j});
+            }
+            if (j + 1 < n && visited.count({i, j + 1}) == 0) {
+                pq.push({a[i] + b[j + 1], {i, j + 1}});
+                visited.insert({i, j + 1});
+            }
+        }
+        return ans;
+    }
+};
+
+// 📌 Total Time Complexity: O(n log n) + O(k log k)
+
+// Space Complexity	O(k)
+
