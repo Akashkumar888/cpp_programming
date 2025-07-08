a582cbf947d3f959aac71581ce25d997c223fed9
Akashkumar888 <akashgkr12@gmail.com>
2025-07-08 10:14:02 +0530

    update

--- DSA_2025/Next element with greater frequency.cpp
+++ DSA_2025/Next element with greater frequency.cpp
@@ -33,3 +33,37 @@ class Solution {
 // So overall space: O(n)
 
 
+
+class Solution {
+  public:
+    vector<int> findGreater(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        vector<int>ans(n);
+        unordered_map<int,int>mp;
+        for(int &num:arr)mp[num]++;
+        stack<int>st;
+        ans[n-1]=-1;
+        st.push(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            while(!st.empty() &&  mp[st.top()]<=mp[arr[i]])st.pop();
+            ans[i]=(st.empty()?-1:st.top());
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};
+
+// ⏱ Time Complexity:
+// Building frequency map: O(n)
+// Main loop (from n-2 to 0):
+// Every element is pushed and popped at most once from the stack.
+// So, all stack operations: O(n) amortized.
+// Total Time Complexity: O(n)
+
+// 💾 Space Complexity:
+// Frequency map: O(n) — in worst case (all elements distinct).
+// Stack: O(n) — in worst case (monotonic decreasing frequency).
+// Answer vector: O(n)
+// Total Space Complexity: O(n)
+
