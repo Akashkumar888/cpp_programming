1a99ffe1ea497b494cc86e27b50117683c1f7e71
Akashkumar888 <akashgkr12@gmail.com>
2025-08-06 16:19:29 +0530

    update

--- DSA_2025/Roman Number to Integer.cpp
+++ DSA_2025/Roman Number to Integer.cpp
@@ -31,6 +31,35 @@ class Solution {
     }
 };
 
+
+class Solution {
+  public:
+  int solve(char &ch){
+      if(ch=='I')return 1;
+      else if(ch=='V')return 5;
+      else if(ch=='X')return 10;
+      else if(ch=='L')return 50;
+      else if(ch=='C')return 100;
+      else if(ch=='D')return 500;
+      else if(ch=='M')return 1000;
+  }
+    int romanToDecimal(string &s) {
+        // code here
+        int n=s.size();
+        if(n==1)return solve(s[0]); // singal char value 
+        int number=0;
+        int i=0;
+        while(i<n-1){
+            if(solve(s[i])<solve(s[i+1]))number-=solve(s[i]);
+            else number+=solve(s[i]);
+            i++;
+        }
+        number+=solve(s[i]);
+        return number;
+    }
+};
+
+
 // Time Complexity:
 // Let n = length of the string s.
 // In the worst case, you iterate through each character once.
