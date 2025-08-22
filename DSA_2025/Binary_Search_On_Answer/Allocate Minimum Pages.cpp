594a427641ba7aba943de9eb8154c3c3d9ac7777
Akashkumar888 <akashgkr12@gmail.com>
2025-08-23 01:00:54 +0530

    update

--- DSA_2025/Binary_Search_On_Answer/Allocate Minimum Pages.cpp
+++ DSA_2025/Binary_Search_On_Answer/Allocate Minimum Pages.cpp
@@ -1,37 +1,36 @@
-
 class Solution {
   public:
-  //minimize the maximum number of pages 
-  // binary search on answer hai 
-  bool isPossible(vector<int>&arr,int k,int mid){
-      int n=arr.size();
-      int cnt=1;
+   // binary search on answer hai 
+  bool isPossible(vector<int>&arr,int mid,int k){
+      int count=1;
       int sum=0;
-      for(int i=0;i<n;i++){
-       sum+=arr[i];
-       if(sum>mid){
-           sum=arr[i];
-           cnt++;
-       }
+      for(int &num:arr){
+          sum+=num;
+          if(sum>mid){
+              count++;
+              sum=num;
+          }
+          if(count>k)return false;
       }
-      return cnt<=k;
+      return true;
   }
     int findPages(vector<int> &arr, int k) {
         // code here
+        // Each student is assigned a contiguous sequence of books. that's why not sort array
         int n=arr.size();
         if(n<k)return -1;
         int l=*max_element(arr.begin(),arr.end());
         int r=accumulate(arr.begin(),arr.end(),0);
-        int ans=r;
+        int minPages=r;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(isPossible(arr,k,mid)){
-                ans=mid;
+            if(isPossible(arr,mid,k)){
+            // minimize the maximum number of pages
+                minPages=mid;
                 r=mid-1;
             }
             else l=mid+1;
         }
-    return ans;    
+        return minPages;
     }
 };
-
