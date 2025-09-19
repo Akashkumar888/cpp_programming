493b41131129e87864f40053db3ba9154dbd8245
Akash kumar <akashgkr12@gmail.com>
2025-09-19 16:51:33 +0530

    Min Add to Make Parentheses Valid

--- DSA_2025/Stack/parentheses/Min Add to Make Parentheses Valid.cpp
+++ DSA_2025/Stack/parentheses/Min Add to Make Parentheses Valid.cpp
@@ -22,3 +22,45 @@ class Solution {
 // Time Complexity → O(n)
 // Space Complexity → O(n)
 
+
+class Solution {
+  public:
+    int minParentheses(string& s) {
+        // code here
+         int n=s.size();
+         int open=0,close=0;
+         // Pass 1: Left to right
+         for(int i=0;i<n;i++){
+             if(s[i]=='(')open++;   // count extra '('
+             else if(s[i]==')' && open>0)open--; // match with an open '('
+         }
+         // Pass 2: Right to left
+         for(int i=n-1;i>=0;i--){
+             if(s[i]==')')close++; // count extra ')'
+             else if(s[i]=='(' && close>0)close--; // match with a close ')'
+         }
+         return open+close;
+    }
+};
+
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+       int open=0,close=0;
+       int n=s.size();
+       for(int i=0;i<n;i++){
+        if(s[i]=='('){
+            open++;
+        }
+        else if(open>0 && s[i]==')'){
+            open--;
+        }
+        else{
+            close++;
+        }
+       } 
+       return close+open;
+    }
+};
+
