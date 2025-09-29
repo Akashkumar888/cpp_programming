6bb9e4967c18d09dad1d245b17124e87eab30279
Akash kumar <akashgkr12@gmail.com>
2025-09-30 04:41:47 +0530

    Generate all binary strings

--- DSA_2025/BackTrack/Generate all binary strings.cpp
+++ DSA_2025/BackTrack/Generate all binary strings.cpp
@@ -26,3 +26,29 @@ class Solution {
     }
 };
 
+// optimized way without sort
+class Solution {
+  public:
+  void solve(int n,int count,string &str,vector<string>&ans){
+      if(count==n){
+          ans.push_back(str);
+          return;
+      }
+      str.push_back('0'); // do push char not string 
+      solve(n,count+1,str,ans);// explore
+      str.pop_back();   // undo       
+      
+      str.push_back('1');// do push char not string 
+      solve(n,count+1,str,ans);// explore
+      str.pop_back();   // undo        
+  }
+    vector<string> binstr(int n) {
+        // code here
+        // Return the strings in  ascending order.
+        // then first 0 after that 1
+        vector<string>ans;
+        string str;
+        solve(n,0,str,ans);
+        return ans;
+    }
+};
