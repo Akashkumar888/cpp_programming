d3090c7f859868fbb7905129bdbedfb1fa667c66
Akash Kumar <143923967+Akashkumar888@users.noreply.github.com>
2026-04-09 01:11:21 +0530

    Update Intersection of Two Sorted Arrays.cpp

--- Akash_DSA_2025_Sheet/Two_Pointer_Approach/Intersection of Two Sorted Arrays.cpp
+++ Akash_DSA_2025_Sheet/Two_Pointer_Approach/Intersection of Two Sorted Arrays.cpp
@@ -56,3 +56,48 @@ class Solution {
         return result;
     }
 };
+
+
+class Solution {
+  public:
+    vector<int> intersection(vector<int>& a, vector<int>& b) {
+        // code here
+        int m=a.size();
+        int n=b.size();
+        vector<int>result;
+        int i=0,j=0;
+        while(i<m && j<n){
+            if(a[i]<b[j])i++;
+            else if(a[i]>b[j])j++;
+            else{
+                if (result.empty() || result.back() != a[i])
+                result.push_back(a[i]);
+                i++; 
+                j++;
+            }
+        }
+        return result;
+    }
+};
+
+class Solution {
+  public:
+    vector<int> intersection(vector<int>& a, vector<int>& b) {
+        // code here
+        int m=a.size();
+        int n=b.size();
+        vector<int>result;
+        int i=0,j=0;
+        while(i<m && j<n){
+            if(a[i]<b[j])i++;
+            else if(a[i]>b[j])j++;
+            else{
+                result.push_back(a[i]);
+                int val = a[i];
+                while (i < m && a[i] == val) i++;
+                while (j < n && b[j] == val) j++;
+            }
+        }
+        return result;
+    }
+};
