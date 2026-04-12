a24c27586932ba3e3ff4e8e34011c411ef2f8a17
Akash Kumar <143923967+Akashkumar888@users.noreply.github.com>
2026-04-12 15:29:03 +0530

    Update Diagonal Traverse (using i+j).cpp

--- Akash_DSA_2025_Sheet/Diagonal_AntiDiagonal/Diagonal Traverse (using i+j).cpp
+++ Akash_DSA_2025_Sheet/Diagonal_AntiDiagonal/Diagonal Traverse (using i+j).cpp
@@ -23,3 +23,38 @@ int main() {
         cout << "\n";
     }
 }
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<vector<int>> mat = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int m = mat.size();
+    int n = mat[0].size();
+
+    // Upper diagonals (including main)
+    for (int col = 0; col < n; col++) {
+        int i = 0, j = col;
+        while (i < m && j < n) {
+            cout << mat[i][j] << " ";
+            i++; j++;
+        }
+        cout << endl;
+    }
+
+    // Lower diagonals
+    for (int row = 1; row < m; row++) {
+        int i = row, j = 0;
+        while (i < m && j < n) {
+            cout << mat[i][j] << " ";
+            i++; j++;
+        }
+        cout << endl;
+    }
+}
