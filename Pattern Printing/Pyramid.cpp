26cbfe7bf79d41568762c067df612f4309984f31
Akash kumar <akashgkr12@gmail.com>
2026-04-19 06:39:41 +0530

    pattern

--- Pattern Printing/Pyramid.cpp
+++ Pattern Printing/Pyramid.cpp
@@ -49,3 +49,162 @@ int main() {
 // 1                 1
 
 
+// SOLID DIAMOND
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n"<<endl;
+    cin>>n;
+
+    // Upper part
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=n-i;k++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+    // Lower part
+    for(int i=n-1;i>=1;i--){
+        for(int k=1;k<=n-i;k++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+// HOLLOW DIAMOND
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n"<<endl;
+    cin>>n;
+
+    // Upper
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=n-i;k++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            if(j==1 || j==i){
+                cout<<i;
+            } else {
+                cout<<" ";
+            }
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+    // Lower
+    for(int i=n-1;i>=1;i--){
+        for(int k=1;k<=n-i;k++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            if(j==1 || j==i){
+                cout<<i;
+            } else {
+                cout<<" ";
+            }
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+// ODD NUMBER STAR PATTERN
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n"<<endl;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        int temp=1;
+        for(int j=1;j<=i;j++){
+            cout<<temp;
+            if(i!=j) cout<<"*";
+            temp+=2;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+// Equilateral Triangle (Centered Star Pyramid)
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter n" << endl;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+
+        // spaces
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // stars (odd numbers: 2*i - 1)
+        for(int j = 1; j <= 2*i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// Hollow Equilateral Triangle
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter n" << endl;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        for(int j = 1; j <= 2*i - 1; j++) {
+            if(j == 1 || j == 2*i - 1 || i == n)
+                cout << "*";
+            else
+                cout << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
